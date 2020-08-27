e574c337549f8c53a57d775c55ebf137b3896a3e
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-08-27 10:42:03 +0200

    add verification token endpoint

--- app.go
+++ app.go
@@ -4,12 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(w, "hello")
+}
+
+func VerificationEndpoint(w http.ResponseWriter, r *http.Request) {
+	challenge := r.URL.Query().Get("hub.challenge")
+	token := r.URL.Query().Get("hub.verifiy_token")
+
+	if token == os.Getenv("VERIFY_TOKEN") {
+		w.WriteHeader(200)
+		w.Write([]byte(challenge))
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("Error, wrong validation token"))
+	}
 }
 
 func main() {
